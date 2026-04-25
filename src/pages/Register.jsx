f94cd2ec436d099b8401f418874ba5b4f4ad41b9
Nikola Nikolov <Extraterrestri0@mail.bg>
2026-04-25 12:46:18 +0300

    Add register functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
-  return <h1>Register Page</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    })
+      .then(() => navigate("/login"))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Register</h2>
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button type="submit">Register</button>
+    </form>
+  );
 }
